f5f3b478a301079c0f472b333a4aa432ca406dc4
Julien Rutscho <58421711+Khoding@users.noreply.github.com>
2020-08-19 10:38:25 +0100

    point neuf fini

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
+import 'package:depenses/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
-import './widgets/user_transaction.dart';
+
+import './models/transaction.dart';
+
+import './widgets/new_transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,15 +17,61 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'Chaussures',
+      amount: 169.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Courses',
+      amount: 128.50,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String newTitle, double newAmount) {
+    final newTransaction = Transaction(
+        title: newTitle,
+        amount: newAmount,
+        date: DateTime.now(),
+        id: DateTime.now().toString());
+
+    setState(() {
+      _userTransactions.add(newTransaction);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return NewTransaction(_addNewTransaction);
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Dépenses personnelles'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -32,11 +82,16 @@ class MyHomePage extends StatelessWidget {
               child: Text('GRAPHIQUES'),
               color: Colors.blue,
             ),
-            Card(
-              child: UserTransaction(),
-            ),
+            TransactionList(_userTransactions),
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          Icons.add,
+        ),
+        onPressed: () => _startAddNewTransaction(context),
       ),
     );
   }
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addNewTransaction;
 
   NewTransaction(this.addNewTransaction);
 
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
   final titleController = TextEditingController();
+
   final amountController = TextEditingController();
 
   void submitData() {
@@ -16,10 +22,12 @@ class NewTransaction extends StatelessWidget {
       return;
     }
 
-    addNewTransaction(
+    widget.addNewTransaction(
       enteredTitle,
       enteredAmount,
     );
+
+    Navigator.of(context).pop();
   }
 
   @override
